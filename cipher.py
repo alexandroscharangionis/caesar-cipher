6f50353d2aa26e87e26a2b79a51593be32fadfb2
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-09 19:46:44 +0200

    merged encrypt and decrypt functions into single caesar_cipher function + refactored while loop

--- cipher.py
+++ cipher.py
@@ -4,35 +4,28 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def encrypt(text, shift_amount):
-    encrypted_text = ''
+def caesar_cipher(text, shift_amount, operation):
+    output_text = ''
     for letter in text:
-        original_index = alphabet.index(letter)
-        new_index = original_index + shift_amount
-        new_letter = alphabet[new_index]
-        encrypted_text += new_letter
-    print(f"The encrypted text is '{encrypted_text}'")
-
-
-def decrypt(encrypted_text, shift_amount):
-    decrypted_text = ''
-    for letter in encrypted_text:
         current_index = alphabet.index(letter)
-        original_index = current_index - shift_amount
-        new_letter = alphabet[original_index]
-        decrypted_text += new_letter
-    print(f"The decrypted text is '{decrypted_text}'")
+        new_index = 0
+        if operation == 'encode':
+            new_index = current_index + shift_amount
+        elif operation == 'decode':
+            new_index = current_index - shift_amount
+        new_letter = alphabet[new_index]
+        output_text += new_letter
+    print(
+        f"The {'encrypted' if operation == 'encode' else 'decrypted'} text is '{output_text}'")
 
 
 while True:
     operation = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    if operation == 'encode':
-        encrypt(text, shift)
-        break
-    elif operation == 'decode':
-        decrypt(text, shift)
-        break
-    else:
+
+    if operation != 'decode' and operation != 'encode':
         print('Input not recognized')
+    else:
+        caesar_cipher(text, shift, operation)
+        break
