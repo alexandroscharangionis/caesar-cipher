48d60fa5b022dbe90cc6587a0d38234e98f06b16
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-09 18:11:07 +0200

    doubled alphabet + created encrypt fn with initial logic

--- cipher.py
+++ cipher.py
@@ -1,7 +1,21 @@
+# Alphabet is doubled so if we try to shift letters towards the end of the alphabet, new_index variable works and doesn't trigger "list out of range" error
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def encrypt(text, shift_amount):
+    encrypted_text = ''
+    for letter in text:
+        original_index = alphabet.index(letter)
+        new_index = original_index + shift_amount
+        new_letter = alphabet[new_index]
+        encrypted_text += new_letter
+    print(f"The encrypted text is {encrypted_text}")
 
 
 operation = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+encrypt(text=text, shift_amount=shift)
